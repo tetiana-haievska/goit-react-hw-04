5c678b3bd0f5559800e6f859bbdfeaa3636f7f61
Tetiana <tatiana.makarenko94@gmail.com>
2025-02-07 20:56:50 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,23 @@
-import { Circles } from "react-loader-spinner";
+// import { Circles } from "react-loader-spinner";
+// import styles from "./Loader.module.css";
+
+// const Loader = () => {
+//   return (
+//     <div className={styles.loaderContainer}>
+//       <Circles visible={true} color="#007bff" height={80} width={80} />
+//     </div>
+//   );
+// };
+
+// export default Loader;
+
+import { ClipLoader } from "react-spinners";
 import styles from "./Loader.module.css";
 
 const Loader = () => {
   return (
     <div className={styles.loaderContainer}>
-      <Circles visible={true} color="#007bff" height={80} width={80} />
+      <ClipLoader color="#00BFFF" size={50} />
     </div>
   );
 };
